7f1ab6fb08ce84fd2e683e30ce69c49d0d25518f
Axel Fernandez <axelfernandez@eventbrite.com>
2020-05-20 20:38:39 -0300

    Adding allowed host

--- deliveryLavalle_site/settings.py
+++ deliveryLavalle_site/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = '9e+mjri=s6f0ttenw4wkligc3o!i%s__9()rv6uykt%krkp(j7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['delivery-lavalle.herokuapp.com']
 
 
 # Application definition
