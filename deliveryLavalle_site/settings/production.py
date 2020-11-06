eb67b2d12513953348b415da694100be02640537
AxelFernandez <axel.fernandez0145@gmail.com>
2020-11-06 15:13:53 -0300

    Configure Droplet

--- deliveryLavalle_site/settings/production.py
+++ deliveryLavalle_site/settings/production.py
@@ -130,7 +130,7 @@ DATABASES = {
 # DATABASES['default'].update(DB_FROM_ENV)
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = '/home/axel/DeliveryLavalle/deliveryLavalle_site/settings/static'
+STATICFILES_DIRS = '/home/axel/DeliveryLavalle/static'
 
 
 # Password validation
