1a0a5e192a7fc7d05570b65830c8614c227d4745
AxelFernandez <axel.fernandez0145@gmail.com>
2021-02-03 15:50:36 -0300

    Adding Debug in QA

--- deliveryLavalle_site/settings/qa.py
+++ deliveryLavalle_site/settings/qa.py
@@ -36,7 +36,7 @@ MELI_TOKEN = get_env_variable('MELI_TOKEN')
 SECRET_KEY = '9e+mjri=s6f0ttenw4wkligc3o!i%s__9()rv6uykt%krkp(j7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['delivery-lavalle.herokuapp.com', 'delivery-lavalle-sandbox.herokuapp.com',
                  'deliverylavalle.com.ar', 'www.deliverylavalle.com.ar',
