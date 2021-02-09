8cbe06c34b2282cdd1f562d6e9a81e76a0288cf3
AxelFernandez <axel.fernandez0145@gmail.com>
2021-02-09 17:54:25 -0300

    Update static files

--- deliveryLavalle_site/settings/qa.py
+++ deliveryLavalle_site/settings/qa.py
@@ -128,8 +128,11 @@ WSGI_APPLICATION = 'deliveryLavalle_site.wsgi_qa.application'
 
 
 DATABASES = {'default': dj_database_url.config()}
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
